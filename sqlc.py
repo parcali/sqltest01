7385be9ee2feed456865f5417fae7dbfe6aef14c
ahmet gunel <ahmetgunel@gmail.com>
2015-11-04 09:13:20 +0300

    sqlc eklendi

---
+++ sqlc.py
@@ -0,0 +1,18 @@
+# INSERT COMMAND
+
+# import the sqlite3 library
+import sqlite3
+
+with sqlite3.connect("new.db") as connection:
+	c = connection.cursor()
+
+	#insert multiple values as tuple
+	cities = [
+	('Boston','MA',600000),('Chicago','IL',2700000),
+	('Houston','TX',2100000),('Phonex','AZ',1500000)
+	]
+
+	#insert data into table
+	c.executemany("INSERT INTO population VALUES(?,?,?)",cities)
+
+
